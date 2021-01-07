481f076b0783922695b56d0a5b7d528f9cf65670
Afoizax <77098890+Afoizax@users.noreply.github.com>
2021-01-07 10:43:58 +0100

    Add files via upload

---
+++ pfcj.js
@@ -0,0 +1,12 @@
+function choix(user) {
+        choixpc = Math.floor(Math.random() * Math.floor(3))
+        console.log(choixpc)
+
+        var choixuser = document.getElementById(user).value
+        console.log(choixuser)
+    }
+
+
+function choixpc() {
+    
+}
